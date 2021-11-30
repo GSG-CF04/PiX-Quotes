0394fde67a692bae82745919a6fcea49f31bd93c
kakashii7 <speed20112@gmail.com>
2021-12-01 00:48:41 +0200

    save buttons name in local storage Relate #7

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const abstractBtn = document.getElementById("abstract-btn");
+const natureBtn = document.getElementById("nature-btn");
+const cosmoBtn = document.getElementById("cosmo-btn");
+const minimalBtn = document.getElementById("minimal-btn");
+abstractBtn.addEventListener("click", buttonName)
+natureBtn.addEventListener("click", buttonName)
+cosmoBtn.addEventListener("click", buttonName)
+minimalBtn.addEventListener("click",buttonName)
+
+
+function buttonName(event){
+    localStorage.setItem('name', event.target.name)
+    }
+    
